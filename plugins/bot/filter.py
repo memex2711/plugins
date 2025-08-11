fc4f814f5a0e16458e4cb7b50d06f28c245b9f13
memex2711 <projectmemex7@gmail.com>
2025-08-11 17:40:05 +0700

    update plugins/bot/filter.py

--- plugins/bot/filter.py
+++ plugins/bot/filter.py
@@ -15,17 +15,17 @@ from os import getenv
 load_dotenv()
 
 from dotenv import load_dotenv
-from ChampuMusic import app
+from MoonMusic import app
 from utils.error import capture_err
 from utils.permissions import adminsOnly, member_permissions
-from ChampuMusic.utils.keyboard import ikb
+from MoonMusic.utils.keyboard import ikb
 from .notes import extract_urls
-from ChampuMusic.utils.functions import (
+from MoonMusic.utils.functions import (
     check_format,
     extract_text_and_keyb,
     get_data_and_name,
 )
-from ChampuMusic.utils.database import (
+from MoonMusic.utils.database import (
     deleteall_filters,
     get_filter,
     get_filters_names,
