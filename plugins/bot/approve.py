b83126db9fbcaa9c8501e77a39dae3d887897ff8
memex2711 <projectmemex7@gmail.com>
2025-08-11 19:01:08 +0700

    update plugins/bot/approve.py

--- plugins/bot/approve.py
+++ plugins/bot/approve.py
@@ -2,10 +2,10 @@ from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter
 from pyrogram.types import ChatJoinRequest
 from pyrogram.errors.exceptions.bad_request_400 import UserAlreadyParticipant
-from ChampuMusic import app
-from ChampuMusic.core.mongo import mongodb
-from ChampuMusic.misc import SUDOERS
-from ChampuMusic.utils.keyboard import ikb
+from MoonMusic import app
+from MoonMusic.core.mongo import mongodb
+from MoonMusic.misc import SUDOERS
+from MoonMusic.utils.keyboard import ikb
 from utils.permissions import adminsOnly, member_permissions
 
 approvaldb = mongodb.autoapprove
